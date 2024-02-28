5d1548714b7fcd6920f26d0dab4df78abd5ee709
trandung2801 <trananhdung280103@gmail.com>
2024-02-28 09:52:10 +0700

    update CRUD View

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,4 +4,8 @@ const handleLoginAPI = (userEmail, userPassword) => {
   return axios.post("/api/login", { email: userEmail, password: userPassword });
 };
 
-export { handleLoginAPI };
+const getAllUsers = (inputId) => {
+  return axios.get(`/api/get-all-users?id=${inputId}`);
+};
+
+export { handleLoginAPI, getAllUsers };
